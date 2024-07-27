2c49c01135ba6a99b1f0f048e425d36d0d6c8330
Remy Blank <remy@c-space.org>
2024-07-27 17:42:10 +0200

    Amélioration des noms de pas du workflow de déploiement.

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,8 @@
 # Copyright 2024 Remy Blank <remy@c-space.org>
 # SPDX-License-Identifier: CC-BY-NC-SA-4.0
 
+import os
+
 project = "Informatique"
 html_title = "Informatique"
 copyright = "2024 Caroline Blank"
@@ -18,6 +20,13 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.todo',
+    # TODO: sphinx_exercise force "numfig = True", et la numérotation ne peut
+    #       pas être désactivée par type, page ou bloc
+    #       <https://github.com/sphinx-doc/sphinx/issues/10316>.
+    # 'sphinx_exercise',
+    # TODO: sphinx_proof force "numfig = True", et empêche la parallélisation
+    #       de la compilation.
+    # 'sphinx_proof',
     'sphinx_togglebutton',
 ]
 myst_enable_extensions = [
@@ -45,6 +54,8 @@ exclude_patterns = [
     'Thumbs.db',
 ]
 
+# if os.environ.get('GITHUB_JOB') == 'deploy-github-pages':
+#     html_baseurl = 'https://t-doc.org/informatique/'
 html_theme = 'sphinx_book_theme'
 html_theme_options = {
     'use_sidenotes': True,
