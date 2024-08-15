199a5adefe04f7bf75b710d7d8172fb443c0a8f0
Remy Blank <remy@c-space.org>
2024-08-15 17:41:31 +0200

    Ajout d'un lien en bas de page sur la license.

--- docs/conf.py
+++ docs/conf.py
@@ -7,9 +7,16 @@ import time
 from tdoc.common.defaults import *
 
 project = "Informatique"
-copyright = f"{time.strftime('%Y')} Caroline Blank, CC-BY-NC-SA-4.0"
+copyright = f"{time.strftime('%Y')} Caroline Blank"
 
 html_title = project
 html_short_title = html_title
 
 keep_warnings = True
+
+html_show_copyright = False
+html_theme_options['extra_footer'] = f"""\
+Copyright {copyright}, \
+<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">\
+CC-BY-NC-SA-4.0</a>\
+"""
