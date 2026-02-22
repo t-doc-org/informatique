018867a76ca9dd06a9a7f528ea4f774fd7f286e3
Caroline Blank <caro@c-space.org>
2026-02-22 14:36:51 +0100

    Contrôle de la validité des entrées des élèves.

--- docs/_static/tdoc/exec-pnm.js
+++ docs/_static/tdoc/exec-pnm.js
@@ -1,6 +1,7 @@
 // Copyright 2026 Caroline Blank <caro@c-space.org>
 // SPDX-License-Identifier: CC-BY-NC-SA-4.0
 
+import {strToInt} from './core.js';
 import {Runner, UserError} from './exec.js';
 
 const largeur = 20;
@@ -10,7 +11,11 @@ const formats = {
         nb_meta: 3,
         vpp: 1,
         couleur: (encodage, index) => {
-            return encodage[index] === '1' ? '#000000':'#ffffff';
+            const v = strToInt(encodage[index]);
+            if (v === undefined || (v !== 0 && v !== 1)) {
+                throw new UserError("Valeur de pixel non valide.");
+            }
+            return v === 1 ? '#000000':'#ffffff';
         },
     },
      'P2': {
@@ -34,6 +39,14 @@ ${nuance(encodage[index + 2], encodage[3])}`;
 };
 
 function nuance(v, max) {
+    max = strToInt(max);
+    if (max === undefined || max <= 0) {
+        throw new UserError("Valeur maximale non valide.");
+    }
+    v = strToInt(v);
+    if (v === undefined || v < 0 || v > max) {
+        throw new UserError("Valeur de pixel non valide.");
+    }
     return Math.round(v * 255 / max).toString(16).padStart(2, '0');
 }
 
@@ -63,17 +76,24 @@ class PnmRunner extends Runner {
         const encodage = text.trim().split(/[\n ]+/);
         const {nb_meta, vpp, couleur} = formats[encodage[0]] ?? {};
         if (!nb_meta) throw new UserError("Erreur dans le format.");
-        const w = largeur * encodage[1], h = largeur * encodage[2];
-        if (encodage.length !== encodage[1] * encodage[2] * vpp + nb_meta) {
+        const w = strToInt(encodage[1]);
+        if (w === undefined || w <= 0) {
+            throw new UserError("Largeur non valide.");
+        }
+        const h = strToInt(encodage[2]);
+        if (h === undefined || h <= 0) {
+            throw new UserError("Hauteur non valide.");
+        }
+        if (encodage.length !== w * h * vpp + nb_meta) {
             throw new UserError(`\
 Le nombre de données ne correspond pas aux dimensions de l'image annoncée.`);
         }
         let svg = `\
-<svg class="pnm" width="${w}" height="${h}"\
+<svg class="pnm" width="${w * largeur}" height="${h * largeur}"\
  xmlns="http://www.w3.org/2000/svg">`;
-        for (let j = 0; j < encodage[2]; j++) {
-            for (let i = 0; i < encodage[1]; i++) {
-                const index = nb_meta + vpp * (j * encodage[1] + i);
+        for (let j = 0; j < h; j++) {
+            for (let i = 0; i < w; i++) {
+                const index = nb_meta + vpp * (j * w + i);
                 svg += `\
 <rect x="${i * largeur}" y="${j * largeur}" width="${largeur}"\
  height="${largeur}" fill="${couleur(encodage, index)}"/>`;
