04c86123cddf77d03bf5fbb45fbffb21bdc4c59e
Caroline Blank <caro@c-space.org>
2026-08-24 14:27:42 +0200

    Modification des valeurs par défaut de JSXGraph.

--- docs/_static.export/jsxgraph.js
+++ docs/_static.export/jsxgraph.js
@@ -14,26 +14,58 @@ attrs.screen = [{
   },
   defaults: {
     arrow: {
+      withlabel: true,
+      fixed: true, highlight: false, showInfobox: false,
       strokeWidth: 2, strokeColor: JXG.palette.black,
+      label: {anchorX: 'middle', anchorY: 'middle', position: '0.4fr left'},
     },
     circle: {
+      fixed: true, highlight: false, showInfobox: false,
       strokeWidth: 2, strokeColor: JXG.palette.blue,
       label: {strokeColor: JXG.palette.blue},
     },
     curve: {
+      fixed: true, highlight: false, showInfobox: false,
       strokeWidth: 2, strokeColor: JXG.palette.blue,
       label: {strokeColor: JXG.palette.blue},
     },
     functiongraph: {
+      withLabel: true,
+      fixed: true, highlight: false, showInfobox: false,
       label: {strokeColor: JXG.palette.blue},
     },
     line: {
+      fixed: true, highlight: false, showInfobox: false,
       strokeWidth: 1, strokeColor: JXG.palette.black,
     },
+    segment: {
+      fixed: true, highlight: false, showInfobox: false,
+      strokeWidth: 2, strokeColor: JXG.palette.black,
+    },
+    angle: {
+      strokeWidth: 1, strokeColor: JXG.palette.black,
+      fillColor: JXG.palette.black, fillOpacity: 0.2,
+      label: {strokeColor: JXG.palette.black,
+      anchorX: 'middle', anchorY: 'middle'},
+    },
     point: {
-      size: 1.5,
+      withLabel: false, size: 0, fixed: true,
+      highlight: false, showInfobox: false,
       strokeWidth: 1, strokeColor: JXG.palette.black,
       fillColor: JXG.palette.black,
+    },
+    glider: {
+      fixed: false, showInfobox: false, highlight: true,
+      size: 2, strokewidth: 1,
+      highlightStrokeWidth: 14, highlightStrokeOpacity: 0.2,
+      highlightStrokeColor: JXG.palette.blue, highlightFillColor: JXG.palette.blue
+    },
+    orthogonalprojection: {
+      withLabel: true, highlight: false, showInfobox: false,
+      strokeColor: JXG.palette.black,
+    },
+    text: {
+      fixed: true,
     },
   },
 }];
@@ -46,9 +78,52 @@ attrs.print = [attrs.nonInteractive, {
     y: {ticks: {insertTicks: false, ticksDistance: 1, minorTicks: 0}},
   },
   defaults: {
-    curve: {strokeWidth: 2, strokeColor: JXG.palette.black},
+    curve: {
+      fixed: true, highlight: false, showInfobox: false,
+      strokeWidth: 2, strokeColor: JXG.palette.black
+    },
+    arrow: {
+      withLabel: true,
+      fixed: true, highlight: false, showInfobox: false,
+      strokeWidth: 2, strokeColor: JXG.palette.black,
+      label: {anchorX: 'middle', anchorY: 'middle', position: '0.4fr left'},
+    },
+    circle: {
+      fixed: true, highlight: false, showInfobox: false,
+      strokeWidth: 2, strokeColor: JXG.palette.black,
+      label: {strokeColor: JXG.palette.black},
+    },
+    curve: {
+      fixed: true, highlight: false, showInfobox: false,
+      strokeWidth: 2, strokeColor: JXG.palette.black,
+      label: {strokeColor: JXG.palette.black},
+    },
+    functiongraph: {
+      withLabel: true,
+      fixed: true, highlight: false, showInfobox: false,
+      label: {strokeColor: JXG.palette.black},
+    },
+    line: {
+      fixed: true, highlight: false, showInfobox: false,
+      strokeWidth: 1, strokeColor: JXG.palette.black,
+    },
+    segment: {
+      fixed: true, highlight: false, showInfobox: false,
+      strokeWidth: 2, strokeColor: JXG.palette.black,
+    },
+    orthogonalprojection: {
+      withLabel: true, highlight: false, showInfobox: false,
+      strokeColor: JXG.palette.black,
+    },
+    angle: {
+      strokeWidth: 1, strokeColor: JXG.palette.black,
+      fillColor: JXG.palette.black, fillOpacity: 0.2,
+      label: {strokeColor: JXG.palette.black,
+      anchorX: 'middle', anchorY: 'middle'},
+    },
     point: {
-      name: '', size: 2,
+      withLabel: false, size: 0, fixed: true,
+      highlight: false, showInfobox: false,
       strokeWidth: 1, strokeColor: JXG.palette.black,
       fillColor: JXG.palette.black,
     },
